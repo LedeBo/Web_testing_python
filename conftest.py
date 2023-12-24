1d370307d213a3ded0ff880c26a5c04ec323beb2
001 <denissova@bk.ru>
2023-12-24 21:28:30 +0300

    added task to hw3

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import pytest, yaml
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+with open('./testdata.yaml', encoding='utf-8') as f:
+    testdata = yaml.safe_load(f)
+    browser = testdata['browser']
+
+
+@pytest.fixture(scope="session")
+def browser():
+    if browser == 'firefox':
+        service = Service(executable_path=GeckoDriverManager().install())
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(service=service, options=options)
+    else:
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service=service, options=options)
+    yield driver
+    driver.quit()
+
+
+    def find_element(self, mode, path):
+        if mode == 'css':
+            element = self.driver.find_element(By.CSS_SELECTOR, path)
+        elif mode == 'xpath':
+            element = self.driver.find_element(By.XPATH, path)
+        else:
+            element = None
+        return element
+
+    def get_element_property(self, mode, path, property):
+        element = self.driver.find_element(mode, path)
+        return element.value_of_css_property(property)
+
+    def close(self):
+        self.driver.close()
